4c9aadb4ac3aed97b3569701f940dd1e66977c75
Dima Kutko <56451967+DimaKutko@users.noreply.github.com>
2022-01-19 18:09:25 +0200

    Init class work 19.01

---
+++ ClassWork1901/app/src/main/java/com/example/classwork1901/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.classwork1901;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
