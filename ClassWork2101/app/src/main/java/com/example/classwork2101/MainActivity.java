1a476f08e92804a9c5de8380b5e1a04589e69f7a
Dima Kutko <56451967+DimaKutko@users.noreply.github.com>
2022-01-21 23:02:45 +0200

    Impl class work

--- ClassWork2101/app/src/main/java/com/example/classwork2101/MainActivity.java
+++ ClassWork2101/app/src/main/java/com/example/classwork2101/MainActivity.java
@@ -2,13 +2,53 @@ package com.example.classwork2101;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int selected;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RadioGroup radio = (RadioGroup) findViewById(R.id.radioGroupSetectTest);
+        selected = radio.getCheckedRadioButtonId();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+
+    public void onClickRadioSelectTest(View v) {
+        selected = v.getId();
+
+        int color;
+
+        switch (selected) {
+            case R.id.yellow:
+                color = Color.YELLOW;
+                break;
+            case R.id.red:
+                color = Color.RED;
+                break;
+            case R.id.green:
+                color = Color.GREEN;
+                break;
+            case R.id.blue:
+                color = Color.BLUE;
+                break;
+            default:
+                color = Color.TRANSPARENT;
+        }
+
+        View view = this.getWindow().getDecorView();
+        view.setBackgroundColor(color);
     }
 }
---
+++ ClassWork2101/app/src/main/java/com/example/classwork2101/RelativeLayoutActivity.java
@@ -0,0 +1,24 @@
+package com.example.classwork2101;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActionBar;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioGroup;
+
+public class RelativeLayoutActivity extends AppCompatActivity {
+    private int selectedTest;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relative_layout);
+    }
+
+
+
+
+}
