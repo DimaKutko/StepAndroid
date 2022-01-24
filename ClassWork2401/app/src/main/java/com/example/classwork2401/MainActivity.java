a9c412e717fd31a91f5e25c163dc463ab3487818
Dima Kutko <56451967+DimaKutko@users.noreply.github.com>
2022-01-24 20:48:37 +0200

    Impl class work

--- ClassWork2401/app/src/main/java/com/example/classwork2401/MainActivity.java
+++ ClassWork2401/app/src/main/java/com/example/classwork2401/MainActivity.java
@@ -1,14 +1,70 @@
 package com.example.classwork2401;
 
+import android.os.Handler;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edit;
+    ListView list;
+
+    ArrayList<String> info;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edit = findViewById(R.id.edit1);
+        list = findViewById(R.id.list1);
+
+        info = new ArrayList<>();
+
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, info);
+        list.setAdapter(adapter);
+
+        edit.setOnKeyListener(new View.OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (event.getAction() == KeyEvent.ACTION_DOWN)
+                    if (keyCode == KeyEvent.KEYCODE_ENTER) {
+                        add(null);
+                        return true;
+                    }
+                return false;
+            }
+        });
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                Toast.makeText(getApplicationContext(), ((TextView) v).getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    public void add(View v) {
+        info.add(0, edit.getText().toString());
+        adapter.notifyDataSetChanged();
+        edit.setText("");
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                edit.requestFocus();
+            }
+        }, 150);
     }
 }
