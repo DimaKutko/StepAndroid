03819ac68ce6c3109b7fcfd7ef2d154b8537f3dd
Dima Kutko <56451967+DimaKutko@users.noreply.github.com>
2022-01-26 19:58:32 +0200

    Init class work part 2

---
+++ ClassWork2901_part2/app/src/main/java/com/example/classwork2901_part2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.classwork2901_part2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
