071a6fd157dc3081915cc78b8c23bac54ba79932
Dima Kutko <56451967+DimaKutko@users.noreply.github.com>
2022-01-30 21:57:02 +0200

    Impl hw

--- homework10/lib/main.dart
+++ homework10/lib/main.dart
@@ -1,6 +1,21 @@
+import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  AwesomeNotifications().initialize(
+    null,
+    [
+      NotificationChannel(
+        channelKey: 'key1',
+        channelName: 'hw_chanel',
+        channelDescription: "Notification example",
+      )
+    ],
+    debug: true,
+  );
+
   runApp(const MyApp());
 }
 
@@ -14,53 +29,55 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  void _onPressed() {
+    AwesomeNotifications().createNotification(
+      content: NotificationContent(
+        id: 1,
+        channelKey: 'key1',
+        title: 'Home work 10',
+        body: 'Android Step',
+        bigPicture: 'https://i.work.ua/employer_gallery/8/4/3/41843.jpg',
+        backgroundColor: Colors.green,
+        notificationLayout: NotificationLayout.BigPicture,
+      ),
+      actionButtons: [
+        NotificationActionButton(key: 'Back', label: '<'),
+        NotificationActionButton(key: 'Play', label: '||'),
+        NotificationActionButton(key: 'Next', label: '>'),
+        NotificationActionButton(key: 'Like', label: '4'),
+      ],
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: const Text('Home Work 10'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+          children: [
+            ElevatedButton(
+              onPressed: _onPressed,
+              child: const Text('Send notification'),
+            )
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
